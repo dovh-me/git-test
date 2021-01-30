d2350084cde44b778b73b8a7fea59055c51d90fc
dovh-me <dovh.me@gmail.com>
2021-01-30 18:14:19 +0530

    Figuring out branching

--- new.py
+++ new.py
@@ -1,6 +1,7 @@
 def main():
     print("Hello World!")
     print("The first change")
+    print("Trying to figure out the branch differences")
 
 if __name__ == "__main__":
     main()
--- readme.txt
+++ readme.txt
@@ -1,2 +1,3 @@
 new line
 second change
+Thrid Change
